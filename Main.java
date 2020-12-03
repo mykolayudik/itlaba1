4131dd2defe4e35b00c5b5d148f97bd054793516
mykolayudik <mykola.yudik.kn.2017@lpnu.ua>
2020-12-04 00:51:25 +0200

    5comit

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        for (int i=1; i<=10;i++){
+            System.out.println("hello world");
+        }
     }
 }
